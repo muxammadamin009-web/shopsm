a820c7e58161675c1ae187550fa83b6450d1c7a1
Muxammadamin Sultanov <muxammadamin009@gmail.com>
2026-08-03 20:55:10 +0500

    Use Brevo SMTP

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,29 +1,21 @@
 import "dotenv/config";
 import nodemailer from "nodemailer";
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
-
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  family: 4,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
   },
 });
 
 export const sendEmail = async (to, subject, html) => {
-
-  await transporter.verify();
-
   await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+    from: `"ShopSM" <${process.env.BREVO_USER}>`,
     to,
     subject,
     html,
   });
-
 };
